fb32afea6426a1ade02a6595d498d6c40d4c94f4
Gauri Shanker <Gaurishanker10@gmail.com>
2024-04-17 19:28:26 +0530

    Completed the animations module of the Maximillian's Course on Udemy

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// disable feature warning
+window.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+import App from "./App.vue";
+import BaseModal from "./components/BaseModal.vue";
+import AllUsers from "./pages/AllUsers.vue";
+import CourseGoals from "./pages/CourseGoals.vue";
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: "/", component: AllUsers },
+		{ path: "/goals", component: CourseGoals },
+	],
+});
+
+const app = createApp(App);
+
+app.component("base-modal", BaseModal);
+app.use(router);
+
+router.isReady().then(() => {
+	app.mount("#app");
+});
+
